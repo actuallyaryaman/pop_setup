99af177ef4f8fdff95051b0e5f0765d476f9f05f
Aryaman <72538446+actuallyaryaman@users.noreply.github.com>
2022-04-02 00:28:31 +0530

    added removal of some unwanted software

--- setup.sh
+++ setup.sh
@@ -13,70 +13,75 @@ sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/p
 apt update
  
 # Get DEB files
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 # Install from Repo
 apt install zsh -y
 apt install code -y
 apt install htop -y
-apt install gnome-tweak-tool -y
-#apt install gparted -y
+#apt install gnome-tweak-tool -y
 apt install simplescreenrecorder -y
 apt install signal-desktop -y
 apt install mpv -y
 apt install telegram-desktop -y
 apt install timeshift -y
+apt install vlc -y
 apt install qt5-style-kvantum -y
-apt install qt5ct -y
+#apt install qt5ct -y
 apt install calibre -y
 #apt install kodi -y
-apt install flameshot -y
+#apt install flameshot -y
 #apt install discord -y #
-apt install gnome-shell-extensions -y
+#apt install gnome-shell-extensions -y
 #apt install delue-gtk deluge -y
-apt install gnome-clocks -y
+#apt install gnome-clocks -y
+apt install libreoffice-qt5 -y
+apt install latte-dock -y
+apt install plasma-wayland-protocols -y
+apt install plasma-workspace-protocols -y
 #apt install gnome-shell-extensions-gpaste -y
 apt install kodi-inputstream-adaptive -y
 apt purge snapd -y
 apt purge gedit -y
-purge pim-sieve-editor -y
-purge evince -y
-purge com.github.donadigo.eddy -y
-purge packagekit -y
-purge plasma-discover-common -y
-purge dragonplayer -y
-purge akregator -y
-purge gnome-boxes -y
-purge konqueror -y
-purge ktnef -y
-purge kmail -y
-purge eog -y
-purge totem -y
-purge simple-scan -y
-purge juk -y
-purge kdepim-runtime -y
-purge gnome-calculator -y
-purge gnome-contacts -y
-purge libreoffice-math -y
-purge plasma-thunderbolt -y
-apt autoremove -y
-purge kup-backup -y
-apt install dolphin -y
-apt install latte-dock -y
-apt install plasma-wayland-protocols -y
-apt install plasma-workspace-protocols -y
+#remove uneeded software:
+apt purge pim-sieve-editor -y
+apt purge evince -y
+apt purge com.github.donadigo.eddy -y
+apt purge packagekit -y
+apt purge plasma-discover-common -y
+apt purge dragonplayer -y
+apt purge akregator -y
+apt purge gnome-boxes -y
+apt purge konqueror -y
+apt purge ktnef -y
+apt purge kmail -y
+apt purge eog -y
+apt purge totem -y
+apt purge simple-scan -y
+apt purge juk -y
+apt purge kdepim-runtime -y
+apt purge gnome-calculator -y
+apt purge gnome-contacts -y
+apt purge libreoffice-gtk3 -y
+apt purge libreoffice-math -y
+apt purge plasma-thunderbolt -y
+apt purge gnome-screenshot -y
+apt purge gnome-accessibility-themes -y
+apt purge kup-backup -y
+apt purge gnome-user-docs -y
 
+#remove uneeded dependencies:
+apt autoremove -y
+apt install dolphin -y
 
 # Install DEB files
-dpkg -i google-chrome-stable_current_amd64.deb
- 
-# Clean up DEB files
-rm -f google-chrome-stable_current_amd64.deb
+#dpkg -i google-chrome-stable_current_amd64.deb
 
-#install oh-my-zsh
-#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+# Clean up DEB files
+#rm -f google-chrome-stable_current_amd64.deb
 
 # Install requirements for kodi-netflix
+
 #apt install python3-pip -y
 #pip install pycryptodomex #(or pip3)
 #pip install websocket-client #(or pip3)
